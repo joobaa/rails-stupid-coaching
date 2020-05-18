1ba9431f305694cb74e341934387123fd6c597ec
Sebastian Jooss <sebastian.jooss@googlemail.com>
2020-05-18 15:38:11 +0200

    installed ask your coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/ask', to: 'questions#ask', as: :ask
+  get '/answer', to: 'answers#answer', as: :answer
 end

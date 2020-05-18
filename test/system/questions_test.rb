ec202affbc953e8ee77d073d4dbd233cb7b9ed0e
Sebastian Jooss <sebastian.jooss@googlemail.com>
2020-05-18 15:48:15 +0200

    added testing and launchy

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -11,5 +11,14 @@ class QuestionsTest < ApplicationSystemTestCase
   test 'visiting /ask renders the form' do
     visit ask_url
     assert_selector 'p', text: 'Ask your coach anything'
+    take_screenshot
+  end
+  test 'saying Hello yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_on 'Ask'
+
+    assert_text "I don't care, get dressed and go to work!"
+    take_screenshot
   end
 end
